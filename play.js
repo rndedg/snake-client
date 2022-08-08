8963d6e794ce47f09cfa2fd7118cdae8b80cff3f
Gord Letkeman <rndedg@gmail.com>
2022-08-08 17:27:55 +0000

    add import/export of setupInput

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const {connect} = require("./client.js");
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = () => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();

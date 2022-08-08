dc718ece1cb81b6066e28d189eda5cbed4018ae3
Gord Letkeman <rndedg@gmail.com>
2022-08-08 17:07:51 +0000

    add on.connect message and snake name

--- client.js
+++ client.js
@@ -13,6 +13,11 @@ const connect = function() {
     console.log(data);
   });
   
+  conn.on("connect", (client) => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SNK");
+  });
+
 
   return conn;
 };

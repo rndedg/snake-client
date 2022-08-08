f0e67e6499189881f9a8f73328d29bac0a51c5b9
Gord Letkeman <rndedg@gmail.com>
2022-08-08 17:21:59 +0000

    add setupInput function and acceptance of ctrl-c key strokes to disconnect

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
 const {connect} = require("./client.js");
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = () => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
 console.log("Connecting ...");
 connect();
